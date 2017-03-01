798e2c31b9a4d3c1c3466942a6d4025ee5cf776c
Andy DuFrene <andy.dufrene@salesforceiq.com>
2017-03-01 12:45:46 -0800

    add failing test

---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class SimpleTest {
+
+    @Test
+    public void test_should_fail() {
+        fail();
+    }
+}
