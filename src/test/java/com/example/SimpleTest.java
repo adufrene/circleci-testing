e4cd8fcc9cc74165b6cadd04ca89a34b6df67433
Andy DuFrene <andy.dufrene@salesforceiq.com>
2017-03-01 13:56:10 -0800

    ignore failing test

--- src/test/java/com/example/SimpleTest.java
+++ src/test/java/com/example/SimpleTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.fail;
@@ -7,6 +8,7 @@ import static org.junit.Assert.fail;
 public class SimpleTest {
 
     @Test
+    @Ignore
     public void test_should_fail() {
         fail();
     }
